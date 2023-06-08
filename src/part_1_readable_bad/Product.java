1ff7ecb710aaf8ea34f8e15a88f99040ac6079f8
Alireza Kafshdartoosi <71192730+AlirezaKToosi@users.noreply.github.com>
2023-06-08 21:20:39 +0200

    Refactor part 1 readable bad

---
+++ src/part_1_readable_bad/Product.java
@@ -0,0 +1,4 @@
+package part_1_readable_bad;
+
+public record Product(String name, String img, String desc, int price, int id, String c) {
+}
--- src/part_1_readable_bad/Project.java
+++ src/part_1_readable_bad/Project.java
@@ -10,22 +10,34 @@ package part_1_readable_bad;
 import java.util.List;
 
 public class Project {
-    String c = "usd"; // currency
-    // inventory
-    List<Inv> inv = List.of(new Inv("Flashlight", "🔦", "A really great flashlight", 100, 1, "usd"),
-            new Inv("Tin can", "🥫", "Pretty much what you would expect from a tin can", 32, 2, "usd"),
-            new Inv("Cardboard Box", "📦", "It holds things", 5, 3, "usd"));
+    private static final String currency = "usd";    // inventory
+    List<Product> inventory = List.of(
+            new Product("Flashlight", "🔦", "A really great flashlight", 100, 1, "usd"),
+            new Product("Tin can", "🥫", "Pretty much what you would expect from a tin can", 32, 2, "usd"),
+            new Product("Cardboard Box", "📦", "It holds things", 5, 3, "usd"));
 
     public Project() {
+        String rowSize = "| %-15s | %-5s | %-50s | %-5d |%n";
+        printHeader(currency);
+        printInventories(rowSize);
+        printFooter();
+    }
+
+    private void printInventories(String format) {
+        for (Product item : inventory) {
+            System.out.format(format, item.name(), item.img(), item.desc(),item.price());
+        }
+    }
+
+    private static void printFooter() {
+        System.out.format("+-----------------+-------+----------------------------------------------------+-------+%n");
+    }
+
+    private static void printHeader(String currency) {
         System.out.println("1 Readable Bad ❌");
-        System.out.println("Global currency: " + c);
-        String format = "| %-15s | %-5s | %-50s | %-5d |%n";
+        System.out.println("Global currency: " + currency);
         System.out.format("+-----------------+-------+----------------------------------------------------+-------+%n");
         System.out.format("| Product         | Image | Description                                        | Price |%n");
-        System.out.format("+-----------------+-------+----------------------------------------------------+-------+%n");
-        for (int i = 0; i < inv.size(); i++) {
-            System.out.format(format, inv.get(i).product(), inv.get(i).img(), inv.get(i).desc(), inv.get(i).price());
-        }
         System.out.format("+-----------------+-------+----------------------------------------------------+-------+%n");
     }
 }
